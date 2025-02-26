4b97f2757d32199435aa834c7a535603cdcc538d
Pon <is02n15ds@bk.ru>
2025-02-26 09:04:46 +0900

    Update laborant_3.py

--- NM/laborant_3.py
+++ NM/laborant_3.py
@@ -1,12 +1,13 @@
 import random
 
+
 # Класс для создания матрицы.
 class Matrix:
     def __init__(self, matrix=None):
         # Инициализация матрицы.
         if matrix:
             self.matrix = matrix
-            self.length, self.width = len(matrix), len(matrix[0]) if matrix else  0
+            self.length, self.width = len(matrix), len(matrix[0]) if matrix else 0
             self.__width_error()
         else:
             if self.__answer():
@@ -15,16 +16,8 @@ class Matrix:
             else:
                 self.matrix, self.length, self.width = self.__generate_matrix()
 
-    def __width_error(self):
-        temp = []
-        for i in range(len(self.matrix)):
-            if len(self.matrix[i]) != self.width:
-                temp.append(i + 1)
-        if temp != []:
-            raise ValueError(
-                f'В матрице не хватает элементов в таких строках: {", ".join([str(elem) for elem in temp])}')
-
-    def __answer(self):
+    @staticmethod
+    def __answer():
         # Задать вопрос пользователю, какую матрицу создать, если он не указал заранее матрицу.
         while True:
             print('Так как вы не указали матрицу, выберите какую матрицу создать?')
@@ -43,7 +36,8 @@ class Matrix:
             else:
                 print('Вы ввели неправильное число!')
 
-    def __matrix_size(self):
+    @staticmethod
+    def __matrix_size():
         # Ввод размера матрицы, если нужно ввести матрицу построчно.
         while True:
             print('Введите высоту матрицы: ')
@@ -67,13 +61,29 @@ class Matrix:
                 continue
             return length, width
 
+    @staticmethod
+    def __generate_matrix():
+        # Рандомная генерация матрицы.
+        length = random.randint(1, 5)
+        width = random.randint(1, 5)
+        return [[random.randint(-10, 10) for _ in range(width)] for _ in range(length)], length, width
+    
+    def __width_error(self):
+        temp = []
+        for i in range(len(self.matrix)):
+            if len(self.matrix[i]) != self.width:
+                temp.append(i + 1)
+        if temp:
+            raise ValueError(
+                f'В матрице не хватает элементов в таких строках: {", ".join([str(elem) for elem in temp])}')
+        
     def __input_matrix(self):
         # Ввод данных для матрицы, построчно.
         matrix = []
         print("Введите элементы матрицы построчно:")
         for i in range(self.length):
             while True:
-                row_input = input(f"Введите {self.width} элементов для строки {i + 1}, через пробел:\n>\t")
+                row_input = input(f"Введите {self.width} элементов для строки {i + 1}, через пробел: \n>\t")
                 try:
                     row = list(map(int, row_input.split()))
                     if len(row) != self.width:
@@ -85,56 +95,44 @@ class Matrix:
                     print("Введены некорректные данные. Попробуйте снова.")
         return matrix
 
-    def __generate_matrix(self):
-        # Рандомная генерация матрицы.
-        length = random.randint(1, 5)
-        width = random.randint(1, 5)
-        return [[random.randint(-10, 10) for _ in range(width)] for _ in range(length)], length, width
-
-    def output_matrix(self):
-        # Вывод матрицы.
+    def __str__(self):
+        # Вывод матрицы и размерности матрицы.
         matrix = "\n".join(["\t".join(map(str, row)) for row in self.matrix])
-        return f'Матрица:\n{matrix}'
+        return f'Матрица: \n{matrix} \nРазмер матрицы: \t{self.length} X {self.width}'
 
-    def output_size(self):
-        # Вывод размера матрицы.
-        return f'Размер матрицы:\t{self.length} X {self.width}'
-
-    def transpose(self):
-        # Транспонирование матрицы.
-        matrix = [list(row) for row in zip(*self.matrix)]
-        self.matrix = matrix
-        self.length, self.width = len(matrix), len(matrix[0]) if matrix else 0
-
-    def multiply_by_number(self, number):
-        # Умножение матрицы на число.
-        matrix = [[element * number for element in row] for row in self.matrix]
-        self.matrix = matrix
-
-    def add(self, other):
+    def __add__(self, other):
         # Сложение матриц.
         if self.length != other.length or self.width != other.width:
             raise ValueError('Матрицы должны быть одного размера для сложения')
-        matrix = [[self.matrix[i][j] + other.matrix[i][j] for j in range(self.width)] for i in range(self.length)]
-        self.matrix = matrix
+        result_matrix = [[self.matrix[i][j] + other.matrix[i][j]
+                          for j in range(self.width)] for i in range(self.length)]
+        return Matrix(result_matrix)
 
-    def subtract(self, other):
+    def __sub__(self, other):
         # Вычитание матриц.
         if self.length != other.length or self.width != other.width:
             raise ValueError('Матрицы должны быть одного размера для вычитания')
-        matrix = [[self.matrix[i][j] - other.matrix[i][j] for j in range(self.width)] for i in range(self.length)]
-        self.matrix = matrix
+        result_matrix = [[self.matrix[i][j] - other.matrix[i][j]
+                          for j in range(self.width)] for i in range(self.length)]
+        return Matrix(result_matrix)
 
-    def multiply(self, other):
+    def __mul__(self, other):
         # Умножение матриц.
         if self.width != other.length:
-            raise ValueError('...')
-        matrix = [[0 for _ in range(other.width)] for _ in range(self.length)]
-        for i in range(self.length):
-            for j in range(other.width):
-                for k in range(self.width):
-                    matrix[i][j] += self.matrix[i][k] * other.matrix[k][j]
-        self.matrix = matrix
+            raise ValueError('Количество столбцов первой матрицы должно быть равно количеству строк второй матрицы')
+        result_matrix = [[self.matrix[i][j] * other.matrix[i][j]
+                          for j in range(self.width)] for i in range(self.length)]
+        return Matrix(result_matrix)
+
+    def transpose(self):
+        # Транспонирование матрицы.
+        transposed_matrix = [list(row) for row in zip(*self.matrix)]
+        return Matrix(transposed_matrix)
+
+    def multiply_by_number(self, number):
+        # Умножение матрицы на число.
+        result_matrix = [[element * number for element in row] for row in self.matrix]
+        return Matrix(result_matrix)
 
 
 # Создаем две матрицы 3x3
@@ -156,46 +154,42 @@ exam_2 = Matrix(matrix_2)
 
 # Вывод матриц
 print("Матрица 1:")
-print(exam_1.output_matrix())
-print(exam_1.output_size())
+print(exam_1)
 
 print("\nМатрица 2:")
-print(exam_2.output_matrix())
-print(exam_2.output_size())
+print(exam_2)
 
 # Транспонирование матрицы 1
-exam_1.transpose()
+exam_10 = exam_1.transpose()
 print("\nТранспонированная матрица 1:")
-print(exam_1.output_matrix())
+print(exam_10)
 
 # Умножение матрицы 2 на число 3
-exam_2.multiply_by_number(3)
+exam_3 = exam_2.multiply_by_number(3)
 print("\nМатрица 2, умноженная на 3:")
-print(exam_2.output_matrix())
+print(exam_3)
 
 # Сложение матриц 1 и 2
-exam_1.add(exam_2)
+exam_4 = exam_1 + exam_2
 print("\nРезультат сложения матриц 1 и 2:")
-print(exam_1.output_matrix())
+print(exam_4)
 
 # Вычитание матриц 1 и 2
-exam_1.subtract(exam_2)
+exam_5 = exam_1 - exam_2
 print("\nРезультат вычитания матриц 1 и 2:")
-print(exam_1.output_matrix())
+print(exam_5)
 
 # Умножение матриц 1 и 2
-exam_1.multiply(exam_2)
+exam_6 = exam_1 * exam_2
 print("\nРезультат умножения матриц 1 и 2:")
-print(exam_1.output_matrix())
+print(exam_6)
 
 # Создание случайной матрицы
 print("\nСоздание случайной матрицы:")
-exam_3 = Matrix()
-print(exam_3.output_matrix())
-print(exam_3.output_size())
+exam_7 = Matrix()
+print(exam_7)
 
 # Создание матрицы вручную
 print("\nСоздание матрицы вручную:")
-exam_4 = Matrix()
-print(exam_4.output_matrix())
-print(exam_4.output_size())
+exam_8 = Matrix()
+print(exam_8)
