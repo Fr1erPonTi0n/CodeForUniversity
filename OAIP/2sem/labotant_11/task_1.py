5a031c1190c6d6321b6607de27139db1c4c98e07
Pon <is02n15ds@bk.ru>
2025-03-11 14:26:37 +0900

    Update task_1.py

--- OAIP/2sem/labotant_11/task_1.py
+++ OAIP/2sem/labotant_11/task_1.py
@@ -1,1 +1,20 @@
+class Widget:
+    pass
 
+class Label(Widget):
+    pass
+
+class LineEdit(Widget):
+    pass
+
+class TextEdit(Widget):
+    pass
+
+class Button(Label):
+    pass
+
+class CheckBox(Button):
+    pass
+
+class RadioButton(Button):
+    pass
