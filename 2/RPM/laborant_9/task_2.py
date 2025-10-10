0a631c00795f92ac9e683cf16235fc1c73f84b4c
Pon <is02n15ds@bk.ru>
2025-10-10 10:33:38 +0900

    Update task_2.py

--- 2/RPM/laborant_9/task_2.py
+++ 2/RPM/laborant_9/task_2.py
@@ -16,7 +16,6 @@ class MainWin(QMainWindow):
 
         self.text = QLabel('Число кликов: 0')
         self.text.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        # он должен быть по центру
 
         self.button = QPushButton("Нажми на меня!")
         self.button.clicked.connect(self.the_button_was_clicked)
@@ -45,3 +44,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
