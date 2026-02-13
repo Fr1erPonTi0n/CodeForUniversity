ff363f5c509b624ed77b0e8079f13377d5289676
Pon <is02n15ds@bk.ru>
2026-02-13 13:10:57 +0900

    Update __init__.py

--- src/2/WEB/laborant_5/api/api_v1/__init__.py
+++ src/2/WEB/laborant_5/api/api_v1/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from .books.views import router as books_router
-from .movies.views import router as movies_router
+from .books import router as books_router
+from .movies import router as movies_router
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(books_router)
