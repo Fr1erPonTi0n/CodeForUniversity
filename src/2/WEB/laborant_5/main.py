2ce72af953ee4bd4e05425f3f00ee41d12e67606
Pon <is02n15ds@bk.ru>
2026-02-13 13:10:22 +0900

    Update main.py

--- src/2/WEB/laborant_5/main.py
+++ src/2/WEB/laborant_5/main.py
@@ -1,10 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
-
 from api import router as api_router
 
 app = FastAPI(title="Books and Movies API")
 app.include_router(api_router)
+
 
 @app.get("/")
 def read_root():
@@ -13,8 +13,6 @@ def read_root():
         "api_v1": "/api/v1",
     }
 
+
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
-
-
-
