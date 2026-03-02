d044139a104876a594a2f87cd028b25d88c53832
Fr1erPonTi0n <is02n15ds@bk.ru>
2026-03-02 13:37:16 +0900

    Создана 7 лабораторная работа 	new file:   src/2/WEB/laborant_7/api/__init__.py 	new file:   src/2/WEB/laborant_7/api/api_v1/__init__.py 	new file:   src/2/WEB/laborant_7/api/api_v1/books/__init__.py 	new file:   src/2/WEB/laborant_7/api/api_v1/books/crud.py 	new file:   src/2/WEB/laborant_7/api/api_v1/books/dependencies.py 	new file:   src/2/WEB/laborant_7/api/api_v1/books/views/__init__.py 	new file:   src/2/WEB/laborant_7/api/api_v1/books/views/details_views.py 	new file:   src/2/WEB/laborant_7/api/api_v1/books/views/list_views.py 	new file:   src/2/WEB/laborant_7/core/__init__.py 	new file:   src/2/WEB/laborant_7/core/config.py 	new file:   src/2/WEB/laborant_7/main.py 	new file:   src/2/WEB/laborant_7/schemas/__init__.py 	new file:   src/2/WEB/laborant_7/schemas/book.py

---
+++ src/2/WEB/laborant_7/core/config.py
@@ -0,0 +1,9 @@
+API_TOKENS = frozenset({
+    "0f-qKfuFyK3YXyDgDUZpZ80jqHuFvnSktFQa40J3VZI",
+    "IdtJ6FJ2rQfOa2MisjVMBGfPxBUk_z5lY1SX18DpSCI"
+})
+
+USERS_DB: dict[str, str] = {
+    'admin': 'admin',
+    'user': '1234',
+}
