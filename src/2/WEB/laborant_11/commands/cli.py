9f61b2a80f08d54bde18ffbd9ae92d430dc537d8
Fr1erPonTi0n <is02n15ds@bk.ru>
2026-03-24 10:46:38 +0900

    Создана 11 лабораторная работа

---
+++ src/2/WEB/laborant_11/commands/cli.py
@@ -0,0 +1,16 @@
+import typer
+from commands.hello import hello_app
+
+app = typer.Typer(
+    no_args_is_help=True,
+    rich_markup_mode='rich'
+)
+
+@app.callback()
+def callback():
+    """
+    This is a sample CLI application.
+    """
+    pass
+
+app.add_typer(hello_app)
